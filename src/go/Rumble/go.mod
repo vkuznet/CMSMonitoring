8d9bc3b41bd90a0107ed2c7feb65998e5079db64
cuzunogl <ceyhunuzngl@gmail.com>
2021-06-08 18:58:56 +0200

    Improve and polish rumble_server

--- src/go/Rumble/go.mod
+++ src/go/Rumble/go.mod
@@ -3,7 +3,10 @@ module rumble_server.go
 go 1.16
 
 require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/gofrs/flock v0.8.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
-	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139 // indirect
 )
--- src/go/Rumble/rumble_server.go
+++ src/go/Rumble/rumble_server.go
@@ -7,12 +7,15 @@ package main
 
 import (
     "bytes"
+    "embed"
     "encoding/json"
     "errors"
     "fmt"
+    "github.com/go-playground/validator"
     "github.com/gofrs/flock"
     "github.com/gorilla/mux"
     "io"
+    "io/fs"
     "io/ioutil"
     "log"
     "net/http"
@@ -25,6 +28,10 @@ import (
 // Global variables
 var portsInUseLockFname string
 
+//go:embed "static"
+var embeddedFiles embed.FS
+
+// ErrorMessage Part of Response struct, provides detailed result
 type ErrorMessage struct {
     Message    string `json:"message"`
     Command    string `json:"command"`
@@ -33,29 +40,41 @@ type ErrorMessage struct {
     Stdout     string `json:"stdout"`
     VerboseMsg string `json:"verbose_stdout"`
 }
+
+// SparkConfRequest Optional part of Request, defines spark processing configurations
 type SparkConfRequest struct {
-    SparkExecutorMemory    string `json:"spark_executor_memory"`
-    SparkExecutorInstances string `json:"spark_executor_instances"`
-    SparkExecutorCores     string `json:"spark_executor_cores"`
-    SparkDriverMemory      string `json:"spark_driver_memory"`
+    SparkExecutorMemory    string `json:"spark_executor_memory" validate:"alphanum,max=3,endswith=g"`
+    SparkExecutorInstances string `json:"spark_executor_instances" validate:"number,max=3"`
+    SparkExecutorCores     string `json:"spark_executor_cores" validate:"number,max=3"`
+    SparkDriverMemory      string `json:"spark_driver_memory" validate:"alphanum,max=3,endswith=g"`
 }
+
+// Request user request which includes JSONiq query and other parameters
 type Request struct {
-    Query      string           `json:"query"`
-    OutputPath string           `json:"output_path"`
-    UserId     string           `json:"userid"`
-    Verbose    int              `json:"verbose"`
+    Query      string           `json:"query" validate:"required,min=3"`
+    OutputPath string           `json:"output_path" validate:"required,min=3"`
+    UserId     string           `json:"userid" validate:"required,min=3"`
+    Verbose    int              `json:"verbose" validate:"oneof=0 1 2"`
     SparkConf  SparkConfRequest `json:"spark_conf"`
 }
+
+// Response provides query result and useful information like spark application id
 type Response struct {
-    UserId            string       `json:"userid"`
+    UserId            string       `json:"userid" `
     SparkAppId        string       `json:"spark_app_id"`
     OutputPath        string       `json:"output_path"`
     ErrorMsg          ErrorMessage `json:"details"`
     InternalQueryFile string       `json:"internal_query_file"`
 }
 
-// runRumble returns Response and isSuccessful
-func runRumble(port1 string, port2 string, queryFile string, outputPath string, userId string, verbose int, sparkConf *SparkConfRequest) (Response, bool) {
+// runRumble
+func runRumble(port1 string,
+    port2 string,
+    queryFile string,
+    outputPath string,
+    userId string,
+    verbose int,
+    sparkConf *SparkConfRequest) (Response, bool) {
     args := []string{
         "--conf", "spark.executor.memory=" + sparkConf.SparkExecutorMemory,
         "--conf", "spark.executor.instances=" + sparkConf.SparkExecutorInstances,
@@ -175,7 +194,11 @@ func sanitizeOutputPath(file string) string {
 // writeQueryToFile Write JSONiq query to a file with unix timestamp.
 func writeQueryToFile(query string, userId string) (string, error) {
     baseQueryPath := "/data/queries/"
-    _ = os.MkdirAll(baseQueryPath, os.ModeDir)   // Create if it does not exist, else do nothing
+    // Create if it does not exist, else do nothing
+    if err := os.MkdirAll(baseQueryPath, os.ModeDir); err != nil {
+        log.Println("[ERROR] could not create temp file, reason:", err)
+        return "", err
+    }
     userId = strings.ReplaceAll(userId, " ", "") // Clear whitespace
     jsoniqFileName := fmt.Sprintf("%s%s-%d.jq", baseQueryPath, userId, time.Now().UnixNano())
     if err := ioutil.WriteFile(jsoniqFileName, []byte(query), 0666); err != nil {
@@ -185,24 +208,34 @@ func writeQueryToFile(query string, userId string) (string, error) {
     return jsoniqFileName, nil
 }
 
-func writeMapToFile(portsInUse map[string]bool, lockFname string) {
-    data, _ := json.Marshal(portsInUse)
-    if err := ioutil.WriteFile(lockFname, data, 0644); err != nil {
-        log.Fatal("Error: cannot write json file", err)
+func writeMapToFile(portsInUse map[string]bool, lockFname string) error {
+    data, err := json.Marshal(portsInUse)
+    if err != nil {
+        log.Println("[ERROR] unable to write ports map to file, reason:", err)
+        return errors.New("unable to write ports map to file")
     }
+    if err := ioutil.WriteFile(lockFname, data, 0644); err != nil {
+        log.Println("[ERROR] unable to write ports map to file, reason:", err)
+        return errors.New("unable to write ports map to file")
+    }
+    return nil
 }
 
 func readMapFromFile(lockFname string) map[string]bool {
     file, _ := ioutil.ReadFile(lockFname)
     data := map[string]bool{}
     if err := json.Unmarshal(file, &data); err != nil {
-        log.Fatal("Error: could not read json file")
+        log.Fatal("[ERROR]: could not read json file")
     }
     return data
 }
 
-func initializePortsInUse(lockFname string) {
+func initializePortsInUse() string {
     portsInUse := make(map[string]bool)
+    lockFile, err := ioutil.TempFile(os.TempDir(), "rumble-ports-*.json")
+    if err != nil {
+        log.Fatalln("[ERROR] unable to create rumble-ports lock file, reason:", err)
+    }
     for _, v := range os.Environ() {
         if strings.Contains(v, "RUMBLE_SERVICE_PORT_PORT_") {
             kv := strings.Split(v, "=")
@@ -210,14 +243,18 @@ func initializePortsInUse(lockFname string) {
         }
     }
     if len(portsInUse) < 2 {
-        log.Fatal("[ERROR] No ports in RUMBLE_SERVICE_PORT_PORT_N format.")
+        log.Fatal("[ERROR] no ports in RUMBLE_SERVICE_PORT_PORT_N format.")
     }
-    writeMapToFile(portsInUse, lockFname)
-    log.Println("[INFO] Total number of spark-submit ports :", len(portsInUse))
-    log.Printf("[INFO] Initial ports in use: %v", fmt.Sprint(portsInUse))
+    if err = writeMapToFile(portsInUse, lockFile.Name()); err != nil {
+        log.Fatalln("[ERROR]", err)
+    }
+    log.Println("[INFO] total number of spark-submit ports :", len(portsInUse))
+    log.Printf("[INFO] initial ports in use: %v", fmt.Sprint(portsInUse))
+    log.Println("[INFO] rumble-ports lock file name:", lockFile.Name())
+    return lockFile.Name()
 }
 
-func releasePorts(port1 string, port2 string, lockFname string) {
+func releasePorts(port1 string, port2 string, lockFname string) error {
     fileLock := flock.New(lockFname)
     var locked bool
     var lockerr error
@@ -227,14 +264,17 @@ func releasePorts(port1 string, port2 string, lockFname string) {
             fmt.Println("[INFO]: got the lock")
             portsInUse := readMapFromFile(lockFname)
             portsInUse[port1], portsInUse[port2] = false, false
-            writeMapToFile(portsInUse, lockFname)
+            if err := writeMapToFile(portsInUse, lockFname); err != nil {
+                _ = fileLock.Unlock()
+                return err
+            }
             _ = fileLock.Unlock()
-            return
+            return nil
         }
         time.Sleep(1 * time.Second)
     }
-    log.Println("[ERROR]: could not get the lock")
-    return
+    return errors.New("could not get the lock")
+
 }
 
 // Check available ports and return if there are any. 2 ports for per spark-submit.
@@ -263,7 +303,10 @@ func reservePorts(lockFname string) (string, string, error) {
                 return "", "", errors.New("no available ports")
             }
             portsInUse[port1], portsInUse[port2] = true, true
-            writeMapToFile(portsInUse, lockFname)
+            if err := writeMapToFile(portsInUse, lockFname); err != nil {
+                _ = fileLock.Unlock()
+                return "", "", err
+            }
             _ = fileLock.Unlock()
             return port1, port2, nil
         }
@@ -281,46 +324,57 @@ func processRumbleServerRequest(w http.ResponseWriter, request *Request, verbose
     if err != nil {
         log.Println("[ERROR] could not create temp JSONiq file, reason:", err)
         w.WriteHeader(http.StatusBadRequest)
-        _ = json.NewEncoder(w).Encode(Response{
+        if encodeErr := json.NewEncoder(w).Encode(Response{
             ErrorMsg: ErrorMessage{
                 Message:  "Internal error. Please try again!",
                 SysError: fmt.Sprintf("%v", err),
             },
-        })
+        }); encodeErr != nil {
+            log.Println("[ERROR] could not marshal response, reason:", encodeErr)
+        }
         return
     }
     port1, port2, err := reservePorts(portsInUseLockFname)
     if err != nil {
         log.Println("[ERROR]", err)
         w.WriteHeader(http.StatusTooManyRequests)
-        _ = json.NewEncoder(w).Encode(Response{
+        if encodeErr := json.NewEncoder(w).Encode(Response{
             ErrorMsg: ErrorMessage{
                 Message:  "All workers are occupied! Please try again!",
                 SysError: fmt.Sprintf("%v", err),
             },
-        })
+        }); encodeErr != nil {
+            log.Println("[ERROR] could not marshal response, reason:", encodeErr)
+        }
         return
     }
-    log.Printf("[INFO] RESERVED PORTS for current request: [%v] - [%v]", port1, port2)
+    log.Printf("[INFO] reserved ports for current request: [%v] - [%v]", port1, port2)
     if request.Verbose > 0 {
         verbose = request.Verbose
     }
-    log.Println("[INFO] Verbosity:", verbose)
+    log.Println("[INFO] verbosity:", verbose)
     userId := strings.ReplaceAll(request.UserId, " ", "") // Clear whitespace
     response, ok := runRumble(port1, port2, jsoniqFileName, request.OutputPath, userId, verbose, &request.SparkConf)
-    log.Printf("[INFO] Response: %#v", response)
-    releasePorts(port1, port2, portsInUseLockFname) // Release reserved ports
-    log.Println("[INFO] Ports released", port1, port2)
+    log.Printf("[INFO] response: %#v", response)
+    // Release reserved ports
+    if err := releasePorts(port1, port2, portsInUseLockFname); err != nil {
+        log.Println("[ERROR] will exit, ports could not released", port1, port2)
+        log.Fatalln("[ERROR]: ", err)
+    }
+    log.Println("[INFO] ports released", port1, port2)
     if !ok {
-        log.Println("[INFO] Fail.")
+        log.Println("[INFO] FAIL")
         w.WriteHeader(http.StatusBadRequest)
-        _ = json.NewEncoder(w).Encode(response)
+        if encodeErr := json.NewEncoder(w).Encode(response); encodeErr != nil {
+            log.Println("[ERROR] could not marshal response, reason:", encodeErr)
+        }
         return
     }
-    //os.Remove(jsoniqFileName) // Remove temp JSONiq file
-    log.Println("[INFO] Success.")
+    log.Println("[INFO] SUCCESS")
     w.WriteHeader(http.StatusOK)
-    _ = json.NewEncoder(w).Encode(response)
+    if encodeErr := json.NewEncoder(w).Encode(response); encodeErr != nil {
+        log.Println("[ERROR] could not marshal response, reason:", encodeErr)
+    }
 }
 
 // RumbleServerRequestHandler we should only receive POST request
@@ -335,28 +389,40 @@ func RumbleServerRequestHandler(w http.ResponseWriter, r *http.Request) {
     log.Printf("[INFO] Request - Method: %v, Host: %v, RemoteAddr: %v, Args: %v, URI: %v",
         r.Method, r.Host, r.RemoteAddr, r.URL, r.RequestURI)
     if err != nil {
-        tempBody, _ := ioutil.ReadAll(r.Body)
+        tempBody, readErr := ioutil.ReadAll(r.Body)
+        if readErr != nil {
+            log.Printf("[ERROR] could not get and even read request body, reasons: %#v, %#v", err, readErr)
+        }
         log.Printf("[ERROR] could not get request body, reason: %v, Body:%#v", err, tempBody)
         w.WriteHeader(http.StatusBadRequest)
-        _ = json.NewEncoder(w).Encode(Response{
+        if encodeErr := json.NewEncoder(w).Encode(Response{
             ErrorMsg: ErrorMessage{
                 Message:  "Please check your query, json could not be decoded!",
                 SysError: fmt.Sprintf("%v", err),
             },
-        })
+        }); encodeErr != nil {
+            log.Println("[ERROR] could not marshal response, reasons:", encodeErr)
+        }
         return
     }
     log.Printf("[INFO] Request Body:%#v", request)
-    // Check user parameters
-    if request.OutputPath == "" || request.UserId == "" || request.Query == "" {
+
+    validate := validator.New()
+    if validationErr := validate.Struct(request); validationErr != nil {
+        log.Println("[ERROR] request body could not be validated", validationErr)
         w.WriteHeader(http.StatusBadRequest)
-        _ = json.NewEncoder(w).Encode(Response{
+        if encodeErr := json.NewEncoder(w).Encode(Response{
             ErrorMsg: ErrorMessage{
-                Message: "'query', 'output_path' or 'userid' cannot be empty!",
+                Message:    "Please check your request, given values are not valid!",
+                SysError:   fmt.Sprintf("%v", err),
+                VerboseMsg: fmt.Sprintf("%v", validationErr),
             },
-        })
+        }); encodeErr != nil {
+            log.Println("[ERROR] could not marshal response, reasons:", encodeErr)
+        }
         return
     }
+
     // Set defaults
     verbose := 0
     if request.SparkConf.SparkExecutorMemory == "" {
@@ -376,15 +442,17 @@ func RumbleServerRequestHandler(w http.ResponseWriter, r *http.Request) {
 
 func handlers() *mux.Router {
     router := mux.NewRouter()
-    fs := http.FileServer(http.Dir("./static"))
-    router.Handle("/", fs)
+    fsys, err := fs.Sub(embeddedFiles, "static")
+    if err != nil {
+        log.Fatalln("[ERROR] embedded files could not initialized, reason:", err)
+    }
+    router.Handle("/", http.FileServer(http.FS(fsys)))
     router.HandleFunc("/rumble-server", RumbleServerRequestHandler).Methods("POST", "OPTIONS")
     return router
 }
 
 func main() {
-    portsInUseLockFname = "/var/lock/rumble-ports.json.lock"
-    initializePortsInUse(portsInUseLockFname)
+    portsInUseLockFname = initializePortsInUse()
     if os.Getenv("MY_NODE_NAME") == "" || os.Getenv("RUMBLE_JAR_FILE") == "" {
         log.Fatal("[ERROR] MY_NODE_NAME and RUMBLE_JAR_FILE env variables are not defined.")
     }
